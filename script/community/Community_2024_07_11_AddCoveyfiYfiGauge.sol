7dc7d9b84b6093b212a42cc0390ac4cfcaa71f5b
Jongseung Lim <penandlim@gmail.com>
2024-07-11 19:34:00 -0400

    chore: add missing call and add comments

--- script/community/Community_2024_07_11_AddCoveyfiYfiGauge.sol
+++ script/community/Community_2024_07_11_AddCoveyfiYfiGauge.sol
@@ -7,6 +7,8 @@ import { YearnStakingDelegate } from "cove-contracts-boosties/src/YearnStakingDe
 import { CoveYearnGaugeFactory } from "cove-contracts-boosties/src/registries/CoveYearnGaugeFactory.sol";
 import { TimelockController } from "@openzeppelin/contracts/governance/TimelockController.sol";
 import { CoveToken } from "cove-contracts-boosties/src/governance/CoveToken.sol";
+import { ERC20RewardsGauge } from "cove-contracts-boosties/src/rewards/ERC20RewardsGauge.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Script is CommunityMultisigScript, StdAssertions {
     function run(bool shouldSend) public override {
@@ -21,41 +23,52 @@ contract Script is CommunityMultisigScript, StdAssertions {
 
         // ================================ START BATCH ===================================
         // Add to batch
+        // Add coveyfi yfi gauge to YSD
         addToBatch(
             yearnStakingDelegate,
             0,
             abi.encodeCall(YearnStakingDelegate.addGaugeRewards, (MAINNET_COVEYFI_YFI_GAUGE, stakingDelegateRewards))
         );
+        // Set gauge reward split
+        addToBatch(
+            yearnStakingDelegate,
+            0,
+            abi.encodeCall(YearnStakingDelegate.setGaugeRewardSplit, (MAINNET_COVEYFI_YFI_GAUGE, 0, 0, 100e16, 0))
+        );
+        // Deploy the reward gauges and the forwarders via the factory
         addToBatch(coveYearnGaugeFactory, 0, abi.encodeCall(CoveYearnGaugeFactory.deployCoveGauges, (newStrategy)));
 
         CoveYearnGaugeFactory.GaugeInfo memory info =
-            CoveYearnGaugeFactory(coveYearnGaugeFactory).getGaugeInfo(yearnGauge);
+            CoveYearnGaugeFactory(coveYearnGaugeFactory).getGaugeInfo(MAINNET_COVEYFI_YFI_GAUGE);
 
         address autoCompoundingGaugeRewardForwarder =
             ERC20RewardsGauge(info.autoCompoundingGauge).getRewardData(address(coveToken)).distributor;
         address nonComoundingGaugeRewardForwarder =
             ERC20RewardsGauge(info.nonAutoCompoundingGauge).getRewardData(address(coveToken)).distributor;
 
+        // Grant manager role to the relayer for reward forwarding
         addToBatch(
             autoCompoundingGaugeRewardForwarder,
             0,
-            abi.encodeCall(YearnStakingDelegate.grantRole, (MANAGER_ROLE, MAINNET_DEFENDER_RELAYER))
+            abi.encodeCall(AccessControl.grantRole, (MANAGER_ROLE, MAINNET_DEFENDER_RELAYER))
         );
+        // Grant manager role to the relayer for reward forwarding
         addToBatch(
             nonComoundingGaugeRewardForwarder,
             0,
-            abi.encodeCall(YearnStakingDelegate.grantRole, (MANAGER_ROLE, MAINNET_DEFENDER_RELAYER))
+            abi.encodeCall(AccessControl.grantRole, (MANAGER_ROLE, MAINNET_DEFENDER_RELAYER))
         );
 
         address[] memory targets = new address[](7);
         uint256[] memory values = new uint256[](7);
         bytes[] memory payloads = new bytes[](7);
 
-        // Grant depositor role to the strategy and the ysd rewards gauge
+        // Grant depositor role to the strategy and non-auto compounding gauge
         targets[0] = yearnStakingDelegate;
-        payloads[0] = abi.encodeCall(YearnStakingDelegate.grantRole, (DEPOSITOR_ROLE, info.coveYearnStrategy));
+        payloads[0] = abi.encodeCall(AccessControl.grantRole, (DEPOSITOR_ROLE, info.coveYearnStrategy));
         targets[1] = yearnStakingDelegate;
-        payloads[1] = abi.encodeCall(YearnStakingDelegate.grantRole, (DEPOSITOR_ROLE, info.nonAutoCompoundingGauge));
+        payloads[1] = abi.encodeCall(AccessControl.grantRole, (DEPOSITOR_ROLE, info.nonAutoCompoundingGauge));
+        // Set deposit limit for the gauge
         targets[2] = yearnStakingDelegate;
         payloads[2] = abi.encodeCall(
             YearnStakingDelegate.setDepositLimit, (MAINNET_COVEYFI_YFI_GAUGE, uint256(100_000e18 * 1e18) / 4529.94e18)
@@ -64,22 +77,30 @@ contract Script is CommunityMultisigScript, StdAssertions {
         require(coveToken.allowedSender(info.nonAutoCompoundingGauge) == false, "Already allowed sender");
         require(coveToken.allowedSender(autoCompoundingGaugeRewardForwarder) == false, "Already allowed sender");
         require(coveToken.allowedSender(nonComoundingGaugeRewardForwarder) == false, "Already allowed sender");
-        targets[3] = coveToken;
+        // Add COVE's allowed sender for the gauges and the forwarders
+        targets[3] = address(coveToken);
         payloads[3] = abi.encodeCall(coveToken.addAllowedSender, (info.autoCompoundingGauge));
-        targets[4] = coveToken;
+        targets[4] = address(coveToken);
         payloads[4] = abi.encodeCall(coveToken.addAllowedSender, (info.nonAutoCompoundingGauge));
-        targets[5] = coveToken;
+        targets[5] = address(coveToken);
         payloads[5] = abi.encodeCall(coveToken.addAllowedSender, (autoCompoundingGaugeRewardForwarder));
-        targets[6] = coveToken;
+        targets[6] = address(coveToken);
         payloads[6] = abi.encodeCall(coveToken.addAllowedSender, (nonComoundingGaugeRewardForwarder));
 
+        // Queue up the timelock transaction
         addToBatch(
             timelock,
             0,
-            abi.encodeCall(TimelockController.scheduleBatch, (targets, values, paylods, bytes32(0), bytes32(0), 2 days))
+            abi.encodeCall(
+                TimelockController.scheduleBatch, (targets, values, payloads, bytes32(0), bytes32(0), 2 days)
+            )
         );
 
         // ================================ TESTING ===================================
+        // Skip ahead to end of timelock period to execute batch
+        vm.warp(block.timestamp + 2 days + 1);
+        vm.prank(MAINNET_COVE_DEPLOYER);
+        TimelockController(payable(timelock)).executeBatch(targets, values, payloads, bytes32(0), bytes32(0));
 
         // ============================= QUEUE UP MSIG ================================
         if (shouldSend) {
