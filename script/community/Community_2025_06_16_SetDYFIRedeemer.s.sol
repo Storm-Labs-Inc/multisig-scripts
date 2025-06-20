5a05a2ed01f827473a1fb3167da4e48114ae7079
lilPlumberBoy <dalymike102@gmail.com>
2025-06-20 10:23:03 -0400

    fix: add kill and remove old contract from master reg

--- script/community/Community_2025_06_16_SetDYFIRedeemer.s.sol
+++ script/community/Community_2025_06_16_SetDYFIRedeemer.s.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.18;
 import { CommunityMultisigScript } from "./CommunityMultisigScript.s.sol";
 import { StdAssertions } from "forge-std/StdAssertions.sol";
 
+interface IDYFIRedeemer {
+    function kill() external;
+}
+
 interface ISwapAndLock {
     function setDYfiRedeemer(address) external;
+    function kill() external;
 }
 
 interface IMasterRegistry {
@@ -17,16 +22,23 @@ contract Script is CommunityMultisigScript, StdAssertions {
         super.run(shouldSend);
         address swapAndLock = 0x9dadf9487737DE29ac685d231bB94348a2635CBb;
         address dyfiRedeemerV2 = 0x0000000000000000000000000000000000000000;
+        address dyfiRedeemer = 0x986F38B5b096070eE64B12Da762468606C8B0706;
         address masterRegistry = 0x91cf20C03bEC656BC008fB2a2177bC3caA34f772;
         // ================================ START BATCH ===================================
         // Add to batch
         addToBatch(swapAndLock, 0, abi.encodeCall(ISwapAndLock.setDYfiRedeemer, dyfiRedeemerV2));
+        // Kill old redeemer
+        addToBatch(dyfiRedeemer, 0, abi.encodeCall(IDYFIRedeemer.kill, ()));
         // Add new contract to master registry
         // TODO: should be a new entry or update the existing one?
         addToBatch(
             masterRegistry,
             0,
             abi.encodeCall(IMasterRegistry.addRegistry, (keccak256("DYFIRedeemerv2"), dyfiRedeemerV2))
+        );
+        // Set old address to 0
+        addToBatch(
+            masterRegistry, 0, abi.encodeCall(IMasterRegistry.addRegistry, (keccak256("DYFIRedeemer"), address(0)))
         );
         // ============================= QUEUE UP MSIG ================================
         if (shouldSend) {
