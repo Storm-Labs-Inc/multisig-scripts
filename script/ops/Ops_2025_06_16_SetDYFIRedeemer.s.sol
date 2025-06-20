55cb6d675d87e6469651045831928a2b1e5c0efe
Jongseung Lim <penandlim@gmail.com>
2025-06-20 15:30:09 -0400

    fix: update strategies too

--- script/ops/Ops_2025_06_16_SetDYFIRedeemer.s.sol
+++ script/ops/Ops_2025_06_16_SetDYFIRedeemer.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-import { CommunityMultisigScript } from "./CommunityMultisigScript.s.sol";
+import { OpsMultisigScript } from "./OpsMultisigScript.s.sol";
 import { StdAssertions } from "forge-std/StdAssertions.sol";
 import { CoveYearnGaugeFactory } from "cove-contracts-boosties/src/registries/CoveYearnGaugeFactory.sol";
 import { YearnGaugeStrategy } from "cove-contracts-boosties/src/strategies/YearnGaugeStrategy.sol";
@@ -21,23 +21,30 @@ interface IMasterRegistry {
     function addRegistry(bytes32, address) external;
 }
 
-contract Script is CommunityMultisigScript, StdAssertions {
+contract Script is OpsMultisigScript, StdAssertions {
     function run() public {
         run(false);
     }
 
     function run(bool shouldSend) public override {
         super.run(shouldSend);
-        address swapAndLock = deployer.getAddress("SwapAndLock");
         address dyfiRedeemerV2 = 0x17072491906E25323175454044642054d97767Fd;
         address dyfiRedeemer = deployer.getAddress("DYFIRedeemer");
+        // Fetch all Yearn gauge info from the factory
+        CoveYearnGaugeFactory factory = CoveYearnGaugeFactory(deployer.getAddress("CoveYearnGaugeFactory"));
+        CoveYearnGaugeFactory.GaugeInfo[] memory gauges = factory.getAllGaugeInfo(100, 0);
 
         // ================================ START BATCH ===================================
-        // Add to batch - update SwapAndLock contract
-        addToBatch(swapAndLock, 0, abi.encodeCall(ISwapAndLock.setDYfiRedeemer, dyfiRedeemerV2));
-
-        // Kill old redeemer
-        addToBatch(dyfiRedeemer, 0, abi.encodeCall(IDYFIRedeemer.kill, ()));
+        // Claim management role from usds strategy
+        address usds1 = deployer.getAddress("YearnGaugeStrategy-yGauge USDS-1 yVault");
+        addToBatch(usds1, 0, abi.encodeCall(ITokenizedStrategy.acceptManagement, ()));
+        // Update every YearnGaugeStrategy with the new (placeholder) redeemer address
+        for (uint256 i = 0; i < gauges.length; i++) {
+            // address management = ITokenizedStrategy(gauges[i].coveYearnStrategy).management();
+            addToBatch(
+                gauges[i].coveYearnStrategy, 0, abi.encodeCall(YearnGaugeStrategy.setDYfiRedeemer, (dyfiRedeemerV2))
+            );
+        }
 
         // ============================= QUEUE UP MSIG ================================
         if (shouldSend) {
